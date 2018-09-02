736d0d4e80043435c366b77269dd1ebc355ad9de
saipaul@hotmail.com <saipaul@hotmail.com>
2018-09-02 23:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ function nowServing (katzDeliLine) {
   if (katzDeliLine.length == 0) {
     return "There is nobody waiting to be served!"
   } else {
-    for (i = 0; i < len; i++) {
-      katzDeliLine.shift()
-      
-      
-    }
-    //katzDeliLine[katzDeliLine.length-1] = ``;
+    katzDeliLine.shift()
+    katzDeliLine[katzDeliLine.length-1] = ``;
   }
   return `Currently serving ${name}.`
 }
